ac9ce8ae73aa6c597ac0c9c78b0dded892b97c71
Tommaso <tommasotroja@outlook.it>
2018-07-23 10:48:06 +0200

    Aggiornamento modelli e home
    Aggiornati i modelli delle classi con istruzioni base per la creazione di hotel, stanze e prenotazioni, correzione typo in homeAlbergatore

--- Booking/Booking/models.py
+++ Booking/Booking/models.py
@@ -12,31 +12,53 @@ class Utente(models.Model):
 class Albergatore(Utente):
     numeroHotel = models.IntegerField()
 
-    def aggiungiHotel(self):
-        pass
+    def aggiungiHotel(self, nome, desc, citta, ind, numstanze):
+        hotel = Hotel()
+        hotel.nomehotel = nome
+        hotel.deschotel = desc
+        hotel.cittahotel = citta
+        hotel.indhotel = ind
+        hotel.numStanze = numstanze
+        hotel.proprietario = self.cognome
 
+    def aggiungiCamera(self, numero, posti, servizi, costo, hotel):
+        stanza = Stanza()
+        stanza.numStanza = numero
+        stanza.postiletto = posti
+        stanza.servizi = servizi
+        stanza.costo = costo
+        stanza.hotelDiAppartenenza = hotel.nomehotel
 
 class Cliente(models.Model):
     email = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
 
-    def effettuaPrenotazione(self):
-        pass
+    def effettuaPrenotazione(self, stanza, data):
+        prenotazione = Prenotazione()
+        prenotazione.cliente = self.email
+        prenotazione.stanza = stanza.numStanza
+        prenotazione.data = data
 
 
 class Hotel(models.Model):
-    nome = models.CharField(max_length=30)
+    nomehotel = models.CharField(max_length=30)
+    deschotel = models.CharField(max_length=100)
+    cittahotel = models.CharField(max_length=30)
+    indhotel = models.CharField(max_length=30)
     numStanze = models.IntegerField()
-    proprietario = Albergatore()
+    proprietario = models.CharField(max_length=30)
 
 
 class Stanza(models.Model):
     numStanza = models.IntegerField()
-    hotelDiAppartenenza = Hotel()
+    postiletto = models.IntegerField()
+    servizi = models.CharField(max_length=100)
+    costo = models.DecimalField(decimal_places=2)
+    hotelDiAppartenenza = models.CharField(max_length=30)
 
 
 class Prenotazione(models.Model):
     idPrenotazione = models.IntegerField()
-    cliente = Cliente()
-    stanza = Stanza()
+    cliente = models.CharField(max_length=30)
+    stanza = models.IntegerField()
     data = models.DateTimeField()
